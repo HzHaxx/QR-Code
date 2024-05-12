ec4ee4412c447efd0afaaa4378a4f8c92968c74c
HzHaxx <hamberzq@gmail.com>
2024-05-12 16:33:30 -0500

    Little Project Finished

---
+++ README.md
@@ -0,0 +1,54 @@
+# Frontend Mentor - QR code component solution
+
+This is a solution to the [QR code component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+> 💡 Important run `npm install` in your terminal to install all the dependencies and `npx gulp` if you want to change something in the sass files. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshots](#screenshots)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### Screenshots
+
+![Desktop](screenshots/desktop.png)
+![Mobile](screenshots/mobile.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox 
+- Desktop-first workflow
+- [Sass](https://sass-lang.com/) - For CSS
+- [Gulp](https://gulpjs.com/) 
+
+### What I learned
+
+I learned how to use the `gulp` task runner to compile `sass` files to `css` files and optimize images. I also learned how to use `gulp` to watch for changes in the `sass` files and automatically compile them to `css` files. 
+
+### Continued development
+
+I'd like in the future use grid to make the layout more responsive and to make the code more clean and organized. 
+I will also implement js to my projects to make them more interactive and dynamic.
+
+## Author
+
+- Website - [Hamber Miguel Zapata Quiroz](https://www.your-site.com)
+- Frontend Mentor - [Hamber](https://www.frontendmentor.io/profile/HzHaxx)
+
---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+import path from 'path' // doesn't need to be installed, it's part of node
+import fs from 'fs' // doesn't need to be installed, it's part of node
+import { glob } from 'glob'
+import { src, dest, watch, series } from 'gulp' 
+import * as dartSass from 'sass' 
+import gulpSass from 'gulp-sass' 
+import sharp from 'sharp' 
+import plumber from 'gulp-plumber' 
+
+const sass = gulpSass(dartSass);
+
+const paths = { 
+    scss: 'src/scss/**/*.scss'
+}
+
+// * Function to compile scss to css
+export function css( done ) { 
+    src(paths.scss, {sourcemaps: true}) 
+        .pipe( plumber() ) 
+        .pipe( sass({ outputStyle: 'compressed' }).on('error', sass.logError) ) 
+        .pipe( dest('./build/css', {sourcemaps: '.'}) ); 
+    done()
+}
+
+// * Functions to process images 
+export async function imagenes(done) { 
+    const srcDir = './src/img';
+    const buildDir = './build/img';
+    const images =  await glob('./src/img/**/*') 
+
+    images.forEach(file => {
+        const relativePath = path.relative(srcDir, path.dirname(file));
+        const outputSubDir = path.join(buildDir, relativePath);
+        procesarImagenes(file, outputSubDir);
+    });
+    done();
+}
+
+function procesarImagenes(file, outputSubDir) {
+    if (!fs.existsSync(outputSubDir)) {
+        fs.mkdirSync(outputSubDir, { recursive: true })
+    }
+    const baseName = path.basename(file, path.extname(file))
+    const extName = path.extname(file)
+
+    if (extName.toLowerCase() === '.svg') {
+        const outputFile = path.join(outputSubDir, `${baseName}${extName}`);
+        fs.copyFileSync(file, outputFile);
+    } else {
+        const outputFile = path.join(outputSubDir, `${baseName}${extName}`);
+        const outputFileWebp = path.join(outputSubDir, `${baseName}.webp`);
+        const outputFileAvif = path.join(outputSubDir, `${baseName}.avif`);
+        const options = { quality: 80 };
+
+        sharp(file).jpeg(options).toFile(outputFile);
+        sharp(file).webp(options).toFile(outputFileWebp);
+        sharp(file).avif().toFile(outputFileAvif);
+    }
+}
+
+// * Function to watch changes in scss files and images
+export function dev() {
+    watch( paths.scss, css );
+    watch('src/img/**/*.{png,jpg}', imagenes) 
+}
+    
+export default series( css, imagenes, dev )
